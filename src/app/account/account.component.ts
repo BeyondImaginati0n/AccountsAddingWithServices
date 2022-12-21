dfe13d30d41f4c50fe2e6f1b977b54a37ac4b6d6
Egor <egors5984@gmail.com>
2022-12-21 20:01:04 +0100

    first commit

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  // providers:[LoggingService]
+})
+export class AccountComponent {
+
+  constructor(private loggingService:LoggingService,private accountsService:AccountsService){
+
+  }
+
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+
+  onSetTo(status: string) {
+   this.accountsService.updateStatus(this.id,status);
+    // this.loggingService.logStatusChanged(status);
+    this.accountsService.statusUpdated.emit(status);
+  }
+}
